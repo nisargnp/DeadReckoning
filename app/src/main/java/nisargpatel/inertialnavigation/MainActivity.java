8ae13a28e0cbc20f7cfb220343883114a103e37e
nnp1997 <nnp1997@gmail.com>
2014-12-16 19:00:15 -0500

    Smaller start and stop buttons, 4 readings for threshold and moving average

--- app/src/main/java/nisargpatel/inertialnavigation/MainActivity.java
+++ app/src/main/java/nisargpatel/inertialnavigation/MainActivity.java
@@ -22,45 +22,67 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     private Sensor androidStepCounter;
     private SensorManager sensorManager;
 
-    private TextView textThresholdSteps;
-    //private TextView textMovingAverageSteps;
+    private TextView textThresholdSteps1;
+    private TextView textThresholdSteps2;
+    private TextView textThresholdSteps3;
+    private TextView textThresholdSteps4;
     private TextView textMovingAverageSteps1;
     private TextView textMovingAverageSteps2;
     private TextView textMovingAverageSteps3;
     private TextView textMovingAverageSteps4;
+
+    private TextView textThresholdSteps;
+    private TextView textMovingAverageSteps;
     private TextView textAndroidSteps;
     private TextView textInstantAcc;
 
-    private static double upperThreshold;
-    private static double lowerThreshold;
+    //private static double upperThreshold;
+    //private static double lowerThreshold;
 
     private Button buttonStartCounter;
     private Button buttonStopCounter;
 
-    private int thresholdStepCount = 0;
+    private int thresholdStepCount1 = 0;
+    private int thresholdStepCount2 = 0;
+    private int thresholdStepCount3 = 0;
+    private int thresholdStepCount4 = 0;
     private int movingAverageStepCount1 = 0;
     private int movingAverageStepCount2 = 0;
     private int movingAverageStepCount3 = 0;
     private int movingAverageStepCount4 = 0;
+
+    private int thresholdStepCount = 0;
+    private int movingAverageStepCount = 0;
     private int androidStepCount = 0;
 
+    ThresholdStepCounter thresholdCountSteps1 = new ThresholdStepCounter(10, 8);
+    ThresholdStepCounter thresholdCountSteps2 = new ThresholdStepCounter(11, 7);
+    ThresholdStepCounter thresholdCountSteps3 = new ThresholdStepCounter(12, 6);
+    ThresholdStepCounter thresholdCountSteps4 = new ThresholdStepCounter(13, 5);
     MovingAverageStepCounter movingAverageCountSteps1 = new MovingAverageStepCounter(0.5);
     MovingAverageStepCounter movingAverageCountSteps2 = new MovingAverageStepCounter(1.0);
     MovingAverageStepCounter movingAverageCountSteps3 = new MovingAverageStepCounter(1.5);
     MovingAverageStepCounter movingAverageCountSteps4 = new MovingAverageStepCounter(2.0);
-    ThresholdStepCounter thresholdCountSteps = new ThresholdStepCounter(upperThreshold, lowerThreshold);
+
+    ThresholdStepCounter thresholdCountSteps = new ThresholdStepCounter(11.5, 6.5);
+    MovingAverageStepCounter movingAverageCountSteps = new MovingAverageStepCounter(.75);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textThresholdSteps = (TextView) findViewById(R.id.textThreshold);
-        //textMovingAverageSteps = (TextView) findViewById(R.id.textMovingAverage);
+        textThresholdSteps1 = (TextView) findViewById(R.id.textT1);
+        textThresholdSteps2 = (TextView) findViewById(R.id.textT2);
+        textThresholdSteps3 = (TextView) findViewById(R.id.textT3);
+        textThresholdSteps4 = (TextView) findViewById(R.id.textT4);
         textMovingAverageSteps1 = (TextView) findViewById(R.id.textA1);
         textMovingAverageSteps2 = (TextView) findViewById(R.id.textA2);
         textMovingAverageSteps3 = (TextView) findViewById(R.id.textA3);
         textMovingAverageSteps4 = (TextView) findViewById(R.id.textA4);
+
+        textThresholdSteps = (TextView) findViewById(R.id.textThreshold);
+        textMovingAverageSteps = (TextView) findViewById(R.id.textMovingAverage);
         textAndroidSteps = (TextView) findViewById(R.id.textAndroid);
         textInstantAcc = (TextView) findViewById(R.id.textInstantAcc);
 
@@ -68,11 +90,10 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         androidStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
+
+
         buttonStartCounter = (Button) findViewById(R.id.buttonStartCounter);
         buttonStopCounter = (Button) findViewById(R.id.buttonStopCounter);
-
-        //Intent intent = new Intent(this, SetThresholds.class);
-        //startActivity(intent);
 
         buttonStartCounter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -132,15 +153,33 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             Double zAcc = (double) event.values[2];
 
-            textInstantAcc.setText(String.valueOf(event.values[2]).substring(0, 6));
+            textInstantAcc.setText(String.valueOf(event.values[2]).substring(0, 5));
 
             if (thresholdCountSteps.stepFound(System.currentTimeMillis(), zAcc)) {
                 thresholdStepCount++;
                 textThresholdSteps.setText(String.valueOf(thresholdStepCount));
             }
-            //if (movingAverageCountSteps.stepFound(System.currentTimeMillis(), zAcc)) {
-            //    movingAverageStepCount++;
-            //    textMovingAverageSteps.setText(String.valueOf(movingAverageStepCount));
+            if (thresholdCountSteps1.stepFound(System.currentTimeMillis(), zAcc)) {
+                thresholdStepCount1++;
+                textThresholdSteps1.setText(String.valueOf(thresholdStepCount1));
+            }
+            if (thresholdCountSteps2.stepFound(System.currentTimeMillis(), zAcc)) {
+                thresholdStepCount2++;
+                textThresholdSteps2.setText(String.valueOf(thresholdStepCount2));
+            }
+            if (thresholdCountSteps3.stepFound(System.currentTimeMillis(), zAcc)) {
+                thresholdStepCount3++;
+                textThresholdSteps3.setText(String.valueOf(thresholdStepCount3));
+            }
+            if (thresholdCountSteps4.stepFound(System.currentTimeMillis(), zAcc)) {
+                thresholdStepCount4++;
+                textThresholdSteps4.setText(String.valueOf(thresholdStepCount4));
+            }
+
+            if (movingAverageCountSteps.stepFound(System.currentTimeMillis(), zAcc)) {
+                movingAverageStepCount++;
+                textMovingAverageSteps.setText(String.valueOf(movingAverageStepCount));
+            }
             if (movingAverageCountSteps1.stepFound(System.currentTimeMillis(), zAcc)) {
                 movingAverageStepCount1++;
                 textMovingAverageSteps1.setText(String.valueOf(movingAverageStepCount1));
@@ -165,9 +204,14 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         }
 
     }
+    public static void setThresholds(double upper, double lower) {
+        //upperThreshold = upper;
+        //lowerThreshold = lower;
+    }
 
-    //decided to implement the buttons in Java code instead of XML
     /*
+    **decided to implement the buttons in Java code instead of XML
+    *
     public void buttonStartCounter (View view) {
         sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -180,9 +224,4 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         sm.unregisterListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
     }
     */
-
-    public static void setThresholds(double upper, double lower) {
-        upperThreshold = upper;
-        lowerThreshold = lower;
-    }
 }
