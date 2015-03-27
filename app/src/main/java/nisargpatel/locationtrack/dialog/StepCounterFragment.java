4f1e163ba283d3ce073af41adc9dc6ba595a8e19
NP <nnp1997@gmail.com>
2015-03-27 06:27:42 -0400

    Created StepCounterCalibrationActivity and StepCounterFragment shells

---
+++ app/src/main/java/nisargpatel/locationtrack/dialog/StepCounterFragment.java
@@ -0,0 +1,38 @@
+package nisargpatel.locationtrack.dialog;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class StepCounterFragment extends DialogFragment{
+
+    private CharSequence[] myList;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        myList = new CharSequence[3];
+        myList[0] = "apple";
+        myList[1] = "potato";
+        myList[2] = "banana";
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Pick one: ")
+                .setItems(myList, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+
+        return builder.create();
+    }
+
+
+}
